0c89507bfa94e161ab93384c3aa901c9591dc40c
Jing Jia <nycjing@gmail.com>
2017-08-09 12:30:19 -0400

    A promise instance

--- src/pledge.js
+++ src/pledge.js
@@ -2,11 +2,34 @@
 /*----------------------------------------------------------------
 Promises Workshop: build the pledge.js ES6-style promise library
 ----------------------------------------------------------------*/
+
 // YOUR CODE HERE:
+function $Promise(executor) {
 
+    if (typeof executor !== 'function') throw new TypeError(/executor.+function/i);
+    this._state = 'pending';
+    this._value;
+    return true;
+};
 
+$Promise.prototype._internalResolve = function (data) {
+    if (this._state === 'pending') {
 
+        this._state = 'fulfilled';
+        this._value = data;
 
+    }
+};
+
+$Promise.prototype._internalReject = function (data) {
+    if (this._state === 'pending') {
+
+        this._state = 'rejected';
+        this._value = data;
+
+    }
+
+};
 
 
 /*-------------------------------------------------------
